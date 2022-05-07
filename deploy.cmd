3c7adc4bbd080f223c444c009e16a454a60b6e6f
Dan Kov <0x666c@mail.ru>
2022-05-07 23:55:52 +0300

    deploy pages

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,9 @@
 call npx vite optimize 
 call npm run build 
 git fetch 
-git checkout --orphan pages 
+git checkout --orphan pages
+git checkout pages
 git add --all :!node_modules 
 git commit -m "deploy pages"
 git push origin pages 
+echo "Done."
